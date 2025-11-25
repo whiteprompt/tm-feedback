6e8ecede3f5580acdad013d9b9c4e3cd061ff2e4
Mariano Selvaggi <mariano.selvaggi81@gmail.com>
2025-11-25 12:48:36 -0300

    fix: Assign user's email to JWT token and disable hardcoded email.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -18,10 +18,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        // token.email = user.email;
+        token.email = user.email;
         token.name = user.name;
         token.image = user.image;
-        token.email = "paula.vergara@whiteprompt.com";
+        // token.email = "paula.vergara@whiteprompt.com";
       }
       return token;
     },
